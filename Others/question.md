db25c8b72d10e15a60cb6e502a1384b3085e7666
kobayashi <jacklink853@gmail.com>
2022-09-08 17:05:11 +0800

    add

---
+++ Others/question.md
@@ -0,0 +1,3 @@
+# win10拒绝访问,需要安全选项卡如何操作？
+
+[解决](https://blog.csdn.net/andymagickai/article/details/123344275)
--- Python/Practice/Practice_5.py
+++ Python/Practice/Practice_5.py
@@ -1,4 +1,14 @@
-def function(data):
-    return 'the data is: %s' % data
+# 老题：输出质数
 
-print(function('hello world'))
+number = int(input("please input a number:"))
+
+i = 2
+while i <= number:
+    j = 2
+    while j <= i**(1/2):
+        if i % j == 0:
+            break
+        j += 1
+    if j > i**(1/2):
+        print(f"{i} ")
+    i += 1
